b10fd2a144a269694795141eac7c786f1d4370fd
Randy <randyfroc@gmail.com>
2019-04-15 09:36:10 -0400

    Added support for MELPA

--- init.el
+++ init.el
@@ -1,9 +1,21 @@
+;; load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t))
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+;;(require 'package)
 (package-initialize)
+;; (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
 
 ;; turn off splash screen
 (setq inhibit-splash-screen t)
