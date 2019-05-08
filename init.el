9fabafcdf04e071b7923b29f50bdbfaeb3e25701
Randy <randyfroc@gmail.com>
2019-05-08 18:19:23 -0400

    added use-package support and applied to web, emmet, and js2 modes

--- init.el
+++ init.el
@@ -5,19 +5,14 @@
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 )
 
-
-
-
-
-
-;; enable autocomplete
-(setq web-mode-ac-sources-alist
-  '(("css" . (ac-source-css-property))
-    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
-
+(require 'use-package)
 
 ;; turn off splash screen
 (setq inhibit-splash-screen t)
+;; show cursor position
+(column-number-mode 1)
+;; save/restore opened files and window config on start
+(desktop-save-mode 1) ;; 0 to turn off
 
 ;; set initial emacs position and size on screen
 (if (display-graphic-p)
@@ -57,7 +52,7 @@
     ("b47946a3a69d091a75b89819983835ac6f7fb94c9b491da38a4907b1185bebe0" default)))
  '(package-selected-packages
    (quote
-    (yasnippet auto-complete web-mode js2-mode bliss-theme))))
+    (use-package yasnippet auto-complete web-mode js2-mode bliss-theme))))
 
 
 (custom-set-faces
@@ -67,14 +62,10 @@
  ;; If there is more than one, they won't work right.
  )
 
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-;; Better imenu
-(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 
-;; save/restore opened files and window config on start
-(desktop-save-mode 1) ;; 0 to turn off
+
+
 
 ;; set theme
 (load-theme 'bliss)
@@ -87,25 +78,36 @@
   (setq-default tab-width 4)
 )
 
-;; show cursor position
-(column-number-mode 1)
+;; enable autocomplete
+(setq web-mode-ac-sources-alist
+  '(("css" . (ac-source-css-property))
+    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+
 
 ;; map just-one-space
 (global-set-key (kbd "C-c x") 'just-one-space);
 
 
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;; (require 'js2-mode)
+;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(use-package js2-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+;; Better imenu
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 
-(require 'emmet-mode)
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook  'emmet-mode)
-(add-hook 'web-mode-hook  'emmet-mode)
-(setq emmet-move-cursor-between-quotes t) ;; default nil
+
+(use-package web-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+
+(use-package emmet-mode
+  :hook (sgml-mode css-mode web-mode)
+  :custom (emmet-move-cursor-between-quotes t))
